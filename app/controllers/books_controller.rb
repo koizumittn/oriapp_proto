ef852e44e8c42deeff3f692d3de983f1da3cb408
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2017-01-08 15:33:43 +0900

    booksコントローラ、BookTestモデルを作成(ActiveRecordを継承していない)。合わせて書籍検索用のビューファイルを作成。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,6 @@
 class BooksController < ApplicationController
+
+  def search
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-resources :top, only: :index
-
+  resources :top, only: :index
+  get 'books/search' => 'books#search'
   root 'top#index'
 
 end
